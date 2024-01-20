92a5be41c3f889a0b84534cebde75acb55983d45
boneyk <124228691+boneyk@users.noreply.github.com>
2024-01-20 16:58:53 +1000

    Delete bd_version/backend directory

--- bd_version/backend/src/main/java/com/example/backend/exceptions/TaskAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.exceptions;
-
-public class TaskAlreadyExistException extends RuntimeException {
-    public TaskAlreadyExistException(String message) {
-        super(message);
-    }
-}
